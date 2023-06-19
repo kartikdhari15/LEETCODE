505f614149156fd84927ee158aa2789dcbc3a347
Kartik Dhari <118604310+kartikdhari15@users.noreply.github.com>
2023-06-19 13:20:58 +0530

    Time: 664 ms (86.57%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -2,9 +2,7 @@
 # select e.name as Employee from Employee e
 # where e.salary>(select salary from Employee where e.managerId=id
 #                and e.managerId is not null);
-
 #SUBQUERIES ARE SLOWER AS TABLE IS FETCHED AGAIN
-
 SELECT a.name AS Employee
 FROM Employee a
 INNER JOIN Employee b
